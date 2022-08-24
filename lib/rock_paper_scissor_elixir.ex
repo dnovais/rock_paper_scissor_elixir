69da34181f9f5685782e6c59f33e4014657e2672
Diego Novais <contato@diegonovais.com.br>
2022-08-24 00:08:00 -0300

    creating the project

---
+++ lib/rock_paper_scissor_elixir.ex
@@ -0,0 +1,18 @@
+defmodule RockPaperScissorElixir do
+  @moduledoc """
+  Documentation for `RockPaperScissorElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RockPaperScissorElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rock_paper_scissor_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule RockPaperScissorElixirTest do
+  use ExUnit.Case
+  doctest RockPaperScissorElixir
+
+  test "greets the world" do
+    assert RockPaperScissorElixir.hello() == :world
+  end
+end
