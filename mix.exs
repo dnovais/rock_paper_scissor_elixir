716828c15bb5c62783bb14a541ea12705e1ba6e9
Diego Novais <contato@diegonovais.com.br>
2022-08-24 00:36:58 -0300

    add static code analysis tool

--- mix.exs
+++ mix.exs
@@ -21,9 +21,8 @@ defmodule RockPaperScissorElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 end
