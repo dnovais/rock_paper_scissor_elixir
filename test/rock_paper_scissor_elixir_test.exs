7fc944bfd4b06415bfc0cd14a6300d30558666be
Diego Novais <contato@diegonovais.com.br>
2022-08-25 00:40:27 -0300

    refactor: rename the main module

--- test/rock_paper_scissor_elixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RockPaperScissorElixirTest do
-  use ExUnit.Case
-  doctest RockPaperScissorElixir
-
-  test "greets the world" do
-    assert RockPaperScissorElixir.hello() == :world
-  end
-end
