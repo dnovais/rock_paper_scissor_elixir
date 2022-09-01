32857128b19365ed3c54721104dc61ef04207dc9
Diego Novais <contato@diegonovais.com.br>
2022-08-31 23:53:15 -0300

    feat: create the tests when players draw

--- test/game_test.exs
+++ test/game_test.exs
@@ -1,3 +1,35 @@
 defmodule GameTest do
   use ExUnit.Case
+  @stone 1
+  @paper 2
+  @scissor 3
+
+  describe "Game.play/2 when the players draw" do
+    test "when all players choose stone" do
+      first_player_choice = @stone
+      second_player_choise = @stone
+
+      assert {:ok, match} = Game.play(first_player_choice, second_player_choise)
+
+      assert match == "Draw!"
+    end
+
+    test "when all players choose paper" do
+      first_player_choice = @paper
+      second_player_choise = @paper
+
+      assert {:ok, match} = Game.play(first_player_choice, second_player_choise)
+
+      assert match == "Draw!"
+    end
+
+    test "when all players choose scissor" do
+      first_player_choice = @scissor
+      second_player_choise = @scissor
+
+      assert {:ok, match} = Game.play(first_player_choice, second_player_choise)
+
+      assert match == "Draw!"
+    end
+  end
 end
