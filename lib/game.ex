97c49ee3549fd03b9248d79aaa1b8bf2074612e9
Diego Novais <contato@diegonovais.com.br>
2022-08-31 23:56:34 -0300

    feat: create the private function and add the conditions to guarantee when game is draw

--- lib/game.ex
+++ lib/game.ex
@@ -8,5 +8,13 @@ defmodule Game do
   @scissor 3
 
   def play(first_player_choice, second_player_choice) do
+    result(first_player_choice, second_player_choice)
+  end
+
+  defp result(first_player_choice, second_player_choice) do
+    cond do
+      first_player_choice == second_player_choice ->
+        {:ok, "Draw!"}
+    end
   end
 end
